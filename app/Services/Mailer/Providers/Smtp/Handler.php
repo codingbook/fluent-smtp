8a052114579353ec096693166bee9382a16455da
Ibrahim Sharif <75041519+aftabshuvo@users.noreply.github.com>
2025-10-18 18:52:30 +0600

    SMTP Logger

--- app/Services/Mailer/Providers/Smtp/Handler.php
+++ app/Services/Mailer/Providers/Smtp/Handler.php
@@ -2,48 +2,108 @@
 
 namespace FluentMail\App\Services\Mailer\Providers\Smtp;
 
-use FluentMail\Includes\Support\Arr;
-use FluentMail\Includes\Core\Application;
 use FluentMail\App\Services\Mailer\BaseHandler;
 use FluentMail\App\Services\Mailer\Providers\Smtp\ValidatorTrait;
+use FluentMail\Includes\Support\Arr;
 
-class Handler extends BaseHandler
-{
+class Handler extends BaseHandler {
     use ValidatorTrait;
 
-    public function send()
-    {
+    private $logger;
+
+    private function log($method, ...$args) {
+        if ($this->logger && method_exists($this->logger, $method)) {
+            call_user_func_array([$this->logger, $method], $args);
+        }
+    }
+
+    public function __construct() {
+        parent::__construct();
+
+        // Initialize logger only if available
+        if (class_exists('FluentMail\App\Services\Mailer\Providers\Smtp\Logger')) {
+            $this->logger = Logger::getInstance();
+        }
+    }
+
+    public function send() {
+        $startTime = microtime(true);
+        $this->log('logFunctionEntry', __CLASS__, __FUNCTION__);
+
         if ($this->preSend()) {
             if ($this->getSetting('auto_tls') == 'no') {
                 $this->phpMailer->SMTPAutoTLS = false;
+                $this->log('logEmailOperation', 'auto_tls_disabled', [
+                    'SMTPAutoTLS' => false
+                ]);
             }
-            return $this->postSend();
+            $result = $this->postSend();
+            $this->log('logFunctionExit', __CLASS__, __FUNCTION__, $result, microtime(true) - $startTime);
+            return $result;
         }
 
-        return $this->handleResponse(new \WP_Error(422, __('Something went wrong!', 'fluent-smtp'), []));
+        $error = new \WP_Error(422, __('Something went wrong!', 'fluent-smtp'), []);
+        $this->log('logError', __CLASS__, __FUNCTION__, 'Pre-send failed', [
+            'wp_error' => $error
+        ]);
+        $this->log('logFunctionExit', __CLASS__, __FUNCTION__, $error, microtime(true) - $startTime);
+        return $this->handleResponse($error);
     }
 
-    protected function postSend()
-    {
+    protected function postSend() {
+        $startTime = microtime(true);
+        $this->log('logFunctionEntry', __CLASS__, __FUNCTION__);
+
         try {
             $this->phpMailer->isSMTP();
-            $this->phpMailer->Host = $this->getSetting('host');
-            $this->phpMailer->Port = $this->getSetting('port');
+            $this->log('logEmailOperation', 'set_smtp_mode', [
+                'mode' => 'SMTP'
+            ]);
 
-            if ($this->getSetting('auth') == 'yes') {
+            $host       = $this->getSetting('host');
+            $port       = $this->getSetting('port');
+            $encryption = $this->getSetting('encryption');
+            $auth       = $this->getSetting('auth') == 'yes';
+
+            $this->phpMailer->Host = $host;
+            $this->phpMailer->Port = $port;
+
+            $this->log('logSmtpConnection', $host, $port, $encryption, $auth);
+
+            if ($auth) {
                 $this->phpMailer->SMTPAuth = true;
-                $this->phpMailer->Username = $this->getSetting('username');
+                $username                  = $this->getSetting('username');
+                $this->phpMailer->Username = $username;
                 $this->phpMailer->Password = $this->getSetting('password');
+
+                $this->log('logSmtpAuth', $username);
             }
 
-            if (($encryption = $this->getSetting('encryption')) != 'none') {
+            if ($encryption != 'none') {
                 $this->phpMailer->SMTPSecure = $encryption;
             }
 
+            // Enable SMTP debug logging
+            if ($this->logger) {
+                $this->phpMailer->SMTPDebug   = 2; // Enable verbose debug output
+                $this->phpMailer->Debugoutput = function ($str, $level) {
+                    $this->logger->addSmtpDebugMessage($str);
+                };
+            }
+
             $fromEmail = $this->phpMailer->From;
+            $fromName  = $this->phpMailer->FromName ?? '';
+
+            $this->log('logEmailOperation', 'sender_setup', [
+                'original_from' => $fromEmail,
+                'from_name'     => $fromName
+            ]);
 
             if ($this->isForcedEmail() && !fluentMailIsListedSenderEmail($fromEmail)) {
                 $fromEmail = $this->getSetting('sender_email');
+                $this->log('logEmailOperation', 'forced_sender_email', [
+                    'new_from' => $fromEmail
+                ]);
             }
 
             if (isset($this->phpMailer->FromName)) {
@@ -54,6 +114,9 @@ class Handler extends BaseHandler
                     $customFrom = $this->getSetting('sender_name')
                 ) {
                     $fromName = $customFrom;
+                    $this->log('logEmailOperation', 'forced_sender_name', [
+                        'new_from_name' => $fromName
+                    ]);
                 }
 
                 $this->phpMailer->setFrom($fromEmail, $fromName);
@@ -61,56 +124,93 @@ class Handler extends BaseHandler
                 $this->phpMailer->setFrom($fromEmail);
             }
 
+            $recipients = [];
             foreach ($this->getParam('to') as $to) {
                 if (isset($to['name'])) {
                     $this->phpMailer->addAddress($to['email'], $to['name']);
+                    $recipients[] = ['email' => $to['email'], 'name' => $to['name'], 'type' => 'to'];
                 } else {
                     $this->phpMailer->addAddress($to['email']);
+                    $recipients[] = ['email' => $to['email'], 'type' => 'to'];
                 }
             }
 
             foreach ($this->getParam('headers.reply-to') as $replyTo) {
                 if (isset($replyTo['name'])) {
                     $this->phpMailer->addReplyTo($replyTo['email'], $replyTo['name']);
+                    $recipients[] = ['email' => $replyTo['email'], 'name' => $replyTo['name'], 'type' => 'reply-to'];
                 } else {
                     $this->phpMailer->addReplyTo($replyTo['email']);
+                    $recipients[] = ['email' => $replyTo['email'], 'type' => 'reply-to'];
                 }
             }
 
             foreach ($this->getParam('headers.cc') as $cc) {
                 if (isset($cc['name'])) {
                     $this->phpMailer->addCC($cc['email'], $cc['name']);
+                    $recipients[] = ['email' => $cc['email'], 'name' => $cc['name'], 'type' => 'cc'];
                 } else {
                     $this->phpMailer->addCC($cc['email']);
+                    $recipients[] = ['email' => $cc['email'], 'type' => 'cc'];
                 }
             }
 
             foreach ($this->getParam('headers.bcc') as $bcc) {
                 if (isset($bcc['name'])) {
                     $this->phpMailer->addBCC($bcc['email'], $bcc['name']);
+                    $recipients[] = ['email' => $bcc['email'], 'name' => $bcc['name'], 'type' => 'bcc'];
                 } else {
                     $this->phpMailer->addBCC($bcc['email']);
+                    $recipients[] = ['email' => $bcc['email'], 'type' => 'bcc'];
                 }
             }
 
-            if ($attachments = $this->getParam('attachments')) {
-                foreach ($attachments as $attachment) {
+            $attachments = [];
+            if ($attachmentsParam = $this->getParam('attachments')) {
+                foreach ($attachmentsParam as $attachment) {
                     $this->phpMailer->addAttachment($attachment[0], $attachment[7]);
+                    $attachments[] = [
+                        'path' => $attachment[0],
+                        'name' => $attachment[7] ?? basename($attachment[0])
+                    ];
                 }
             }
 
-            if ($this->getParam('headers.content-type') == 'text/html' || $this->getParam('headers.content-type') == 'multipart/alternative') {
+            $contentType = $this->getParam('headers.content-type');
+            if ($contentType == 'text/html' || $contentType == 'multipart/alternative') {
                 $this->phpMailer->isHTML(true);
             }
 
-            $this->phpMailer->Subject = $this->getSubject();
+            $subject = $this->getSubject();
+            $body    = $this->getParam('message');
+            $altBody = null;
 
-            $this->phpMailer->Body = $this->getParam('message');
+            $this->phpMailer->Subject = $subject;
+            $this->phpMailer->Body    = $body;
 
-            if ($this->getParam('headers.content-type') == 'multipart/alternative') {
-                $this->phpMailer->AltBody = $this->getParam('alt_body');
+            if ($contentType == 'multipart/alternative') {
+                $altBody                      = $this->getParam('alt_body');
+                $this->phpMailer->AltBody     = $altBody;
                 $this->phpMailer->ContentType = 'multipart/alternative';
             }
+
+            // Log complete email details before sending
+            $this->log('logEmailSend',
+                ['email' => $fromEmail, 'name' => $fromName],
+                $recipients,
+                $subject,
+                $this->getParam('headers'),
+                $body,
+                $altBody
+            );
+
+            $this->log('logEmailOperation', 'email_details', [
+                'content_type'     => $contentType,
+                'has_attachments'  => !empty($attachments),
+                'attachment_count' => count($attachments),
+                'attachments'      => $attachments,
+                'body_encoding'    => $this->phpMailer->Encoding ?? 'unknown'
+            ]);
 
             $this->phpMailer->send();
 
@@ -118,23 +218,45 @@ class Handler extends BaseHandler
                 'response' => 'OK'
             ];
 
+            $this->log('logEmailOperation', 'send_success', [
+                'response' => $returnResponse
+            ]);
+
         } catch (\Exception $e) {
             $returnResponse = new \WP_Error(422, $e->getMessage(), []);
+            $this->log('logError', __CLASS__, __FUNCTION__, $e->getMessage(), [
+                'exception'         => $e,
+                'smtp_debug_output' => $this->logger ? $this->logger->getSmtpDebugOutput() : []
+            ]);
         }
 
         $this->response = $returnResponse;
 
-        return $this->handleResponse($this->response);
+        $result = $this->handleResponse($this->response);
+        $this->log('logFunctionExit', __CLASS__, __FUNCTION__, $result, microtime(true) - $startTime);
+        return $result;
     }
 
-    public function setSettings($settings)
-    {
+    public function setSettings($settings) {
+        $startTime = microtime(true);
+        $this->log('logFunctionEntry', __CLASS__, __FUNCTION__, [
+            'settings_keys' => array_keys($settings)
+        ]);
+
         if (Arr::get($settings, 'key_store') == 'wp_config') {
             $settings['username'] = defined('FLUENTMAIL_SMTP_USERNAME') ? FLUENTMAIL_SMTP_USERNAME : '';
             $settings['password'] = defined('FLUENTMAIL_SMTP_PASSWORD') ? FLUENTMAIL_SMTP_PASSWORD : '';
+
+            $this->log('logEmailOperation', 'settings_from_wp_config', [
+                'username_defined' => defined('FLUENTMAIL_SMTP_USERNAME'),
+                'password_defined' => defined('FLUENTMAIL_SMTP_PASSWORD')
+            ]);
         }
 
         $this->settings = $settings;
+
+        $this->log('logPhpmailerSettings', $settings);
+        $this->log('logFunctionExit', __CLASS__, __FUNCTION__, null, microtime(true) - $startTime);
 
         return $this;
     }
